564d39ca1410c2cd75f466b50d19b346c45bc4d5
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2021-03-26 01:26:44 +0900

    add color patern

--- WifiAndroid02.ino
+++ WifiAndroid02.ino
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <string.h>
 #include <WiFi.h>
  
+const char* ssid = "rx600m-90ebb6-1"; 
+const char* password = "3250d8640b1b7"; 
 
-
-// ウェブサーバーをポート80で開始
 WiFiServer server(80);
 
 #include <Adafruit_NeoPixel.h>
@@ -15,12 +15,15 @@ WiFiServer server(80);
 
 #define NUMPIXELS 91 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 100 // Time (in milliseconds) to pause between pixels
+#define DELAYVAL 1 // Time (in milliseconds) to pause between pixels
 
 int cnt = 0;
+int warmcnt = 0;
+int k = 15;
 int red = 255;
 int green = 255;
 int blue = 255;
+int lightMode = 0;
 
 
 void setup() {
@@ -70,83 +73,33 @@ void runWIFI(){
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
-        if(c != '\n'){
-          currentLine += c;
-        }
+//        Serial.write(c);
         Serial.print("currentLine:");
         Serial.println(currentLine);
-       
-          if(currentLine == "RED"){
-            Serial.println("REDBUTTON");
-            red = 128;
-            green = 0;
-            blue = 0;
-            currentLine = "";
+        if(c == '\n'){
+          Serial.println("runLED");
+          char str[15];
+          strcpy(str,currentLine.c_str());
+          lightMode = atoi(strtok(str, ","));
+          red = atoi(strtok(NULL, ","));
+          green = atoi(strtok(NULL, ","));
+          blue = atoi(strtok(NULL, ","));
+          currentLine = "";
+          if(lightMode == 0){
             runLED();
-            delay(100);
-//            break;
-            }
-          else if(currentLine == "GRN"){
-            Serial.println("GRNBUTTON");
-            red = 0;
-            green = 128;
-            blue = 0;
-            currentLine = "";
-            runLED();
-            delay(100);
-//            break;
-            }
-           else if(currentLine == "BLU"){
-            Serial.println("BLUBUTTON");
-            red = 0;
-            green = 0;
-            blue = 128;
-            currentLine = "";
-            runLED();
-            delay(100);
-//            break;
-            }
-          else if(c == '\n'){
-            }
-//          else if(tmp1 == "R"){
-//            red = 25*atoi(tmp2.c_str());
-//            Serial.println(red);
-//            String tmp1 = "";
-//            String tmp2 = "";
-//            delay(100);
-////            break;
-//            }
-//          else if(tmp1 == "G"){
-//            green = 25*atoi(tmp2.c_str());
-//            Serial.println(green);
-//            String tmp1 = "";
-//            String tmp2 = "";
-//            delay(100);
-//            break;
-//            }
-//          else if(tmp1 == "B"){
-//            blue = 25*atoi(tmp2.c_str());
-//            Serial.println(blue);
-//            String tmp1 = "";
-//            String tmp2 = "";
-//            delay(100);
-//            break;
-//            }
-//          if(currentLine.length() > 3){
-//            String tmp1 = "";
-//            String tmp2 = "";
-//            delay(100);
-//            break;
-//            }
-//          }
+          }
+        }
+        else if (c != '\r'){
+          currentLine += c;
+        }
+      }
+      if(lightMode == 1){
+        runLED();
       }
     }
-    Serial.println("while(client.connect) end");
-//    client.stop();
-//    Serial.println("Dis Connect");
+//    Serial.println("while(client.connect) end");
   }
-  Serial.println("if(client) end");
+//  Serial.println("if(client) end");
 }
 
 void setupLED(){
@@ -156,20 +109,148 @@ void setupLED(){
   
   pixels.begin();
 }
+int rainbow[100][3]={
+  {255, 0, 0},  // red
+  {255, 40, 0},   // orange
+  {255, 155, 0}, //yellow
+  {128, 225, 0},  // light green
+  {0, 255, 0}, // green
+  {0, 128, 255}, // skyblue
+  {0, 0, 255}, // blue
+  {255,0,70}, // purple
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+};
 
 void runLED(){
-  pixels.clear(); // Set all pixel colors to 'off'
-
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  pixels.clear();
+  for(int i=0; i<NUMPIXELS; i++) {
     int num = (i+cnt)%8;
-//    pixels.setPixelColor(i, pixels.Color(rainbow[num][0],rainbow[num][1], rainbow[num][2]));
-      pixels.setPixelColor(i, pixels.Color(red,green,blue));
-    
+    int num2 = (i+cnt)%100;
+    switch(lightMode){
+      case 0:
+//        Serial.println("0");
+        pixels.setPixelColor(i, pixels.Color(red,green,blue));
+        break;
+      case 1:
+//        Serial.println("1");
+        if(red == 1){
+          pixels.setPixelColor(i, pixels.Color(rainbow[num][0],rainbow[num][1], rainbow[num][2]));
+        }
+        else if (green == 1){
+          pixels.setPixelColor(i, pixels.Color(rainbow[num2][0],rainbow[num2][1], rainbow[num2][2]));
+        }
+        else if (blue == 1){
+          pixels.setPixelColor(i, pixels.Color(warmcnt,warmcnt,warmcnt));
+        }
+        delay(1);
+        break;
+      }
   }
     pixels.show();   // Send the updated pixel colors to the hardware.
 
-    delay(DELAYVAL); // Pause before next pass through loop
+//    delay(DELAYVAL); // Pause before next pass through loop
     cnt++;
+    if(lightMode == 1 && blue == 1){
+      if(warmcnt == 255){
+        k = -15;
+        delay(400);
+        }
+      else if(warmcnt == 15){
+        k = 15;
+        delay(400);
+        }
+      warmcnt += k;
+      Serial.println(warmcnt);
+    }
+    delayMicroseconds(100);
 }
